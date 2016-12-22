bfd4122a0c2d6331b46f955e1608fc2a0998426b
tezmo <jvanderroest@gmail.com>
2016-12-22 19:09:23 +0100

    V1

---
+++ WakeUpLight7Seg/WakeUpLight7Seg.ino
@@ -0,0 +1,400 @@
+#include <Button.h>
+#include <DS3232RTC.h>    //http://github.com/JChristensen/DS3232RTC
+#include <Time.h>         //http://www.arduino.cc/playground/Code/Time  
+#include <Wire.h>         //http://arduino.cc/en/Reference/Wire (included with Arduino IDE)
+#include <Encoder.h>
+#include "LedControl.h"   //https://github.com/wayoda/LedControl
+
+#define RotaryAPIN    2
+#define RotaryBPIN    3
+Button encButton(4);
+
+LedControl lc=LedControl(12,11,10,1);
+
+Encoder myEnc(RotaryAPIN, RotaryBPIN);
+long oldPosition  = -999;
+int menuResult = 0;
+boolean alarmHourSet = false;
+boolean timeHourSet = false;
+
+boolean alarmOn = false;
+long alaHours;
+long alaMins;
+long timeHours;
+long timeMins;
+
+boolean debug = false;            // extra debug info?
+long snoozeMillis = 0;          // timekeeper for snoozing checking
+long snoozeTime = 10000;        // time between snoozes
+
+time_t prevDisplay = 0;
+float timeLapse = 0;
+float R;
+int interval;
+int brightness;
+int steps = 10000; // milliseconds between steps
+const int pwmIntervals = 180; //STEPS
+
+
+//Setup the output PWM pin
+const int outputPin = 5;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(outputPin, OUTPUT);
+  pinMode(RotaryAPIN, INPUT_PULLUP);
+  pinMode(RotaryBPIN, INPUT_PULLUP);  
+  encButton.begin();
+  
+  // Calculate the R variable (only needs to be done once at setup)
+  R = (pwmIntervals * log10(2))/(log10(255));
+
+  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  setSyncInterval(60);
+  if(timeStatus() != timeSet) 
+      Serial.println("Unable to sync with the RTC");
+  else
+      Serial.println("RTC has set the system time"); 
+
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+
+}
+
+void loop() {
+
+  tmElements_t tm;
+  time_t t;
+  
+  static enum { TIME, MENU, SETTIME, SETALARM, SHOWALARM, ALARM, SNOOZE } state = TIME;
+  switch (state) {
+      
+    case TIME: {
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+
+        if ( RTC.alarm(ALARM_1)&&alarmOn){
+          state = ALARM;
+        }
+        
+
+        if (encButton.released()){
+          myEnc.write(0);
+          state = MENU;
+        }
+
+        if (now() != prevDisplay) { //update the display only if the time has changed
+            prevDisplay = now();
+            DisplayDigits(hour(),minute());
+            Serial.println();
+        }
+        break;
+      }
+
+
+    case MENU: {
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+        
+        Serial.print("MENU: ");
+        menuResult = readEncoder(3);
+        Serial.println(menuResult);
+
+
+       switch (menuResult) {
+          case 0:
+            Serial.println("SET ALARM");
+            lc.setChar(0,0,'a',false);
+            lc.setChar(0,1,'l',false);
+            lc.setChar(0,2,'a',false);
+            lc.setRow(0,3,B00000101);
+            break;
+          case 1:    
+            Serial.println("RETURN & ALARM OFF");
+            lc.setRow(0,0,B00000000);
+            lc.setRow(0,1,B00000101);
+            lc.setRow(0,2,B01001111);
+            lc.setRow(0,3,B00001111);
+            break;
+          case 2:    
+            Serial.println("TIME SET ");
+            lc.setRow(0,0,B00001111);
+            lc.setRow(0,1,B01011011);
+            lc.setRow(0,2,B01001111);
+            lc.setRow(0,3,B00001111);
+            break;
+          }
+              
+
+
+
+        if (encButton.released()){
+          
+          switch (menuResult) {
+            case 0:
+              Serial.println("SET ALARM");
+              alarmHourSet = false;
+              alaHours = 0;
+              alaMins = 0;
+              state = SETALARM;
+              break;
+            case 1:    
+              Serial.println("TIME & ALARM OFF");
+              alarmOn = false;
+              state = TIME;
+              break;
+            case 2:    
+              Serial.println("SET TIME");
+              timeHourSet = false;
+              timeHours = 0;
+              timeMins = 0;
+              state = SETTIME;
+              break;
+            }
+        }  
+      break;
+      }
+      
+    case SETALARM: {
+         long tempEnc = 0;
+        
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+
+        if (!alarmHourSet){
+          tempEnc = readEncoder(24);
+          Serial.print("Alarm hour: ");
+          Serial.println(tempEnc);
+
+           //leftmost digits - hours
+           lc.setDigit(0,0,(tempEnc / 10),false);
+           lc.setDigit(0,1,(tempEnc % 10),false);
+           lc.setRow(0,2,B00000000);
+           lc.setRow(0,3,B00000000);
+
+          
+          if (encButton.released()){
+            alaHours = tempEnc;
+            myEnc.write(0);
+            alarmHourSet = true;
+            myEnc.write(0);
+          } 
+        }
+        
+        if (alarmHourSet){
+          tempEnc = readEncoder(60);
+          Serial.print("Alarm mins: ");
+          Serial.println(tempEnc);
+
+           //rightmost digits - mins
+           lc.setDigit(0,2,(tempEnc / 10),false);
+           lc.setDigit(0,3,(tempEnc % 10),false);
+
+         
+          if (encButton.released()){
+            alaMins = tempEnc;
+            Serial.println("MINS SET");
+
+             long alaMinsSet = 0;
+             long alaHourSet = 0;
+            
+            //FIX TO START 30 mins EARLIER - START WAKING UP BEFORE ALARM TIME
+            if (alaMins < 30)
+            {
+              alaMinsSet = alaMins + 30;
+              alaHourSet = alaHours -1;
+            } 
+            if (alaMins >= 30)
+            {
+              alaMinsSet = alaMins - 30;
+              alaHourSet = alaHours;
+            }
+            
+            RTC.setAlarm(ALM1_MATCH_HOURS, 0, alaMinsSet, alaHourSet, 0);
+            alarmOn = true;
+            state = SHOWALARM;  
+          }
+        }
+        break;
+      }
+
+    case SHOWALARM: {
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+        Serial.print("ALARM IS SET TO: ");
+        Serial.print(alaHours);
+        Serial.print(":");
+        Serial.println(alaMins);  
+        lc.setIntensity(0,0); 
+        delay(500);
+        lc.setIntensity(0,8); 
+        DisplayDigits(alaHours,alaMins);
+        delay(2000);
+        state = TIME;
+        break;
+      }
+
+
+    case SETTIME: {
+        long tempEnc = 0;
+        
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+
+        if (!timeHourSet){
+          
+          tempEnc = readEncoder(24);
+          Serial.print("Time hour: ");
+          Serial.println(tempEnc);
+           
+           //leftmost digits - hours
+           lc.setDigit(0,0,(tempEnc / 10),false);
+           lc.setDigit(0,1,(tempEnc % 10),false);
+           lc.setRow(0,2,B00000000);
+           lc.setRow(0,3,B00000000);
+           
+          if (encButton.released()){
+            timeHours = tempEnc;
+            timeHourSet = true;
+            myEnc.write(0);
+          } 
+        }
+        
+        if (timeHourSet){
+          tempEnc = readEncoder(60);
+          Serial.print("Time mins: ");
+          Serial.println(tempEnc);
+
+            //rightmost digits - mins
+           lc.setDigit(0,2,(tempEnc / 10),false);
+           lc.setDigit(0,3,(tempEnc % 10),false);
+          
+          if (encButton.released()){
+            timeMins = tempEnc;
+            Serial.println("MINS SET");
+
+       
+            tm.Year = 46; //since 1970, this means 2016
+            tm.Month = 12;
+            tm.Day = 3;
+            tm.Hour = timeHours;
+            tm.Minute = timeMins;
+            tm.Second = 0;
+            t = makeTime(tm);
+            RTC.set(t);        //use the time_t value to ensure correct weekday is set
+            setTime(t);        
+            lc.setIntensity(0,0);
+            delay(500);
+            lc.setIntensity(0,8);            
+            if (debug){ 
+              Serial.println("SET TIME to: ");
+
+                        
+              //TimeDateDisplay();
+              DisplayDigits(hour(),minute());
+            }
+            state = TIME;  
+          }
+        }
+        break;
+      }
+
+    case ALARM: {
+        if(debug){    
+        Serial.print("state = ");
+        Serial.println(state);
+        }
+        
+        if ((millis() - timeLapse) > steps) {
+          wakeUp();
+        }
+
+        DisplayDigits(hour(),minute());
+
+       if (encButton.released()){
+        brightness = 0;
+        analogWrite(outputPin, brightness);
+        alarmOn = false;
+        state = TIME;
+       }
+    break;
+    }
+
+    //NOT USED
+   case SNOOZE: {
+        if(debug){    
+        Serial.println("state = snooze");
+        }
+ 
+       // User ihas indicated is still awake
+       if (millis() - snoozeMillis> snoozeTime){
+         state = ALARM;
+       }
+      break;
+    }
+
+    
+  }
+  delay(10); 
+}
+
+
+void DisplayDigits(long value1, long value2)
+{
+
+
+  
+   //leftmost digits - hours
+   lc.setDigit(0,0,(value1 / 10),false);
+   lc.setDigit(0,1,(value1 % 10),false);
+   
+   //right most digits / minutes)
+   lc.setDigit(0,2,(value2 / 10),false);
+   lc.setDigit(0,3,(value2 % 10),alarmOn);
+}
+
+
+
+
+long readEncoder(long maxOptions){
+  //because one 'blip' is 4 
+  maxOptions = maxOptions * 4;
+  
+  long newPosition = myEnc.read();
+  
+  if (newPosition > maxOptions) { newPosition = 0; myEnc.write(0);}
+  if (newPosition < 0) { newPosition = maxOptions; myEnc.write(maxOptions);}
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+  }
+  
+  return oldPosition/4;
+  
+}
+
+void wakeUp(){
+        if (interval < pwmIntervals){
+          interval=interval+1;
+        }
+        
+        brightness = pow (2, (interval / R)) - 1;
+        // Set the LED output to the calculated brightness
+        analogWrite(outputPin, brightness);
+        Serial.print("Brightness: ");
+        Serial.println(brightness);     
+        timeLapse = millis();
+}
